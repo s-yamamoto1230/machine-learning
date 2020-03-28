924394ba5dad32f65c949d88a21e2a854a1f7fa6
yamamoto <sky.shu4230@gmail.com>
2020-03-28 14:43:31 +0900

    欠損地処理

---
+++ sample13.py
@@ -0,0 +1,23 @@
+#欠損地処理  NaN
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({'Age':[12,14,12,18],
+                    'Gender':["M","M","F","F"],
+                    'Height':[np.nan,160,150,175],
+                    'Weight':[35,50,np.nan,60]})
+print(df)
+
+#欠損地確認
+print(df.isnull())  #欠損部分はtureで出てくる
+
+#欠損の数を求める
+print(df.isnull().sum())    #tureの数を数える
+
+#欠損地のある行を削除(あまり使わない)  dropna()
+df_dropna = df.dropna()
+print(df_dropna)
+
+#欠損地の書き換え  fillna(maxやmeanなど)
+df_fillna = df.fillna(df.mean())    #欠損地を中央値に書き換え
+print(df_fillna)

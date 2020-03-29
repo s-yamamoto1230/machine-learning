da96310c558319c99955fd9f246e5657639effda
yamamoto <sky.shu4230@gmail.com>
2020-03-29 18:12:52 +0900

    Matplotlibの基本

--- sample15.py
+++ sample15.py
@@ -13,9 +13,27 @@ plt.show()              #グラフ呼び出し
 x = np.linspace(0,10,20)
 print(x)
 y = x**2
-z = ((x-2)*(x-4))**2
+z = (x-1)**3
+w = ((x-2)*(x-4))**2
 plt.plot(x,y,label="y=x^2")
-plt.plot(x,y,label="z=(x-1)^3")
-plt.plot(x,y,label="w=((x-2)(x-4))^2")
+plt.plot(x,z,label="z=(x-1)^3")
+plt.plot(x,w,label="w=((x-2)(x-4))^2")
 plt.legend()
+plt.show()
+
+#グラフを分けて書く  subplot(行,列,表示する位置)
+plt.figure(figsize=(12,4))
+#1列目に表示
+plt.subplot(1,3,1)
+plt.plot(x,y,label="y=x^2")
+plt.legend()
+#2列目に表示
+plt.subplot(1,3,2)
+plt.plot(x,z,label="z=(x-1)^3")
+plt.legend()
+#3列目に表示
+plt.subplot(1,3,3)
+plt.plot(x,w,label="w=((x-2)(x-4))^2")
+plt.legend()
+plt.tight_layout()  #グラフ同士の重なりを防ぐ
 plt.show()
